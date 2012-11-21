55921a0cad6cc51efedc68a5069e2a9f841859bb
胡峰 <hufeng@funguide.com.cn>
2012-11-21 17:41:03 +0800

    添加语音例程

---
+++ HFFrame/Example/VoiceReadViewController.h
@@ -0,0 +1,15 @@
+//
+//  VoiceReadViewController.h
+//  HFFrame
+//
+//  Created by jason on 12-11-21.
+//  Copyright (c) 2012年 jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HFBaseViewController.h"
+#import "iFlyMSC/IFlySynthesizerControl.h"
+
+@interface VoiceReadViewController : HFBaseViewController
+
+@end
