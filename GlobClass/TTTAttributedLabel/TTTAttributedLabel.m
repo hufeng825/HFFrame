f31647d9124293e3a0bd8a579d6e2220595f8346
胡峰 <hufeng@funguide.com.cn>
2012-11-25 03:13:24 +0800

    修改ttattributedlabel warning警告

--- GlobClass/TTTAttributedLabel/TTTAttributedLabel.m
+++ GlobClass/TTTAttributedLabel/TTTAttributedLabel.m
@@ -577,6 +577,8 @@ static inline NSAttributedString * NSAttributedStringByScalingFontSize(NSAttribu
                 [self.delegate attributedLabel:self didSelectLinkWithDate:result.date];
             }
             break;
+        default:
+            break;
     }
 }
 
