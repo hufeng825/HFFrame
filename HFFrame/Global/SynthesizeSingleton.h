50e52ff41a1f44b64095ee070b92894495fffcb0
胡峰 <hufeng@funguide.com.cn>
2012-12-03 17:00:18 +0800

    添加宏定义单例

---
+++ HFFrame/Global/SynthesizeSingleton.h
@@ -0,0 +1,71 @@
+
+/*
+SYNTHESIZE_SINGLETON_FOR_CLASS(HFItem);
+
++ (HFItem *) sharedInstance {
+    BOOL mustInitialize = !sharedHFItem;
+    HFItem *v = [HFItem sharedHFItem];
+    if (mustInitialize)
+    {
+        //可以放入您需要的第一次初始化内容
+    }
+    return v;
+}
+*/
+
+
+
+#define SYNTHESIZE_SINGLETON_FOR_CLASS(classname) \
+ \
+static classname *shared##classname = nil; \
+ \
++ (classname *)shared##classname \
+{ \
+	@synchronized(self) \
+	{ \
+		if (shared##classname == nil) \
+		{ \
+			shared##classname = [[self alloc] init]; \
+		} \
+	} \
+	 \
+	return shared##classname; \
+} \
+ \
++ (id)allocWithZone:(NSZone *)zone \
+{ \
+	@synchronized(self) \
+	{ \
+		if (shared##classname == nil) \
+		{ \
+			shared##classname = [super allocWithZone:zone]; \
+			return shared##classname; \
+		} \
+	} \
+	 \
+	return nil; \
+} \
+ \
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+	return self; \
+} \
+ \
+- (id)retain \
+{ \
+	return self; \
+} \
+ \
+- (NSUInteger)retainCount \
+{ \
+	return NSUIntegerMax; \
+} \
+ \
+- (oneway void)release \
+{ \
+} \
+ \
+- (id)autorelease \
+{ \
+	return self; \
+}
