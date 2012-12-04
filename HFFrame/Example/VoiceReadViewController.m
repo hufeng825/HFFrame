55737358a5758fe90428dde0af722fb2dbef70e8
胡峰 <hufeng@funguide.com.cn>
2012-12-04 15:11:54 +0800

    修改语音播报崩溃bug

--- HFFrame/Example/VoiceReadViewController.m
+++ HFFrame/Example/VoiceReadViewController.m
@@ -13,7 +13,7 @@
 @end
 
 @implementation VoiceReadViewController
-
+IFlySynthesizerControl *_iflySynizeControl;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -29,19 +29,24 @@
     // Do any additional setup after loading the view from its nib.
     /********************----播报---******************///
     NSString *initParam = [[[NSString alloc] initWithFormat:@"appid=%@",APPID]autorelease];
-    IFlySynthesizerControl *_iflySynizeControl = [[IFlySynthesizerControl alloc] initWithOrigin:H_CONTROL_ORIGIN initParam:initParam];
+    _iflySynizeControl = [[IFlySynthesizerControl alloc] initWithOrigin:H_CONTROL_ORIGIN initParam:initParam];
     [self.view addSubview:_iflySynizeControl];
     // Configure the RecognizeControl
     // 设置语音识别控件的参数,具体参数可参看开发文档
     [_iflySynizeControl setText:@"白日依山尽,黄河入海流,欲穷千里目,更上一层楼  北京新空气" params:nil ];
     _iflySynizeControl.delegate = self;
     [_iflySynizeControl setVoiceName:@"xiaoyu"];
-    [initParam release];
-    [_iflySynizeControl setShowUI:NO];
+    [_iflySynizeControl setShowUI:YES];
     [_iflySynizeControl setSpeed:100];
     [_iflySynizeControl start];
     /***********************************************/
 
+}
+-(void)viewDidDisappear:(BOOL)animated
+{
+    [_iflySynizeControl  cancel];
+    _iflySynizeControl.delegate = nil;
+    [super viewDidDisappear:YES];
 }
 #pragma 合成接口实现
 - (void)onSynthesizerEnd:(IFlySynthesizerControl *)iFlySynthesizerControl theError:(SpeechError) error
