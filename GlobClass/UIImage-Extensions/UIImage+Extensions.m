8fc9c7922b5d775741cc2e43aa22c137877d2feb
胡峰 <hufeng@funguide.com.cn>
2013-03-20 13:29:03 +0800

    添加图片体积压缩函数 用户上传图片

--- GlobClass/UIImage-Extensions/UIImage+Extensions.m
+++ GlobClass/UIImage-Extensions/UIImage+Extensions.m
@@ -15,6 +15,9 @@
 #define toCF (CFTypeRef)
 #endif
 
+#define MAX_IMAGEPIX 200.0          // max pix 200.0px
+#define MAX_IMAGEDATA_LEN 50000.0   // max data length 5K
+
 
 CGFloat DegreesToRadians(CGFloat degrees) {return degrees * M_PI / 180;};
 CGFloat RadiansToDegrees(CGFloat radians) {return radians * 180/M_PI;};
@@ -279,6 +282,75 @@ static UIImage *animatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceRe
     return animatedImageWithAnimatedGIFReleasingImageSource(CGImageSourceCreateWithURL(toCF url, NULL), duration);
 }
 
+
+- (UIImage *)compressedImage {
+    CGSize imageSize = self.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    
+    if (width <= MAX_IMAGEPIX && height <= MAX_IMAGEPIX) {
+        // no need to compress.
+        return self;
+    }
+    
+    if (width == 0 || height == 0) {
+        // void zero exception
+        return self;
+    }
+    
+    UIImage *newImage = nil;
+    CGFloat widthFactor = MAX_IMAGEPIX / width;
+    CGFloat heightFactor = MAX_IMAGEPIX / height;
+    CGFloat scaleFactor = 0.0;
+    
+    if (widthFactor > heightFactor)
+        scaleFactor = heightFactor; // scale to fit height
+    else
+        scaleFactor = widthFactor; // scale to fit width
+    
+    CGFloat scaledWidth  = width * scaleFactor;
+    CGFloat scaledHeight = height * scaleFactor;
+    CGSize targetSize = CGSizeMake(scaledWidth, scaledHeight);
+    
+    UIGraphicsBeginImageContext(targetSize); // this will crop
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.size.width  = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [self drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    //pop the context to get back to the default
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+    
+}
+
+- (NSData *)compressedData:(CGFloat)compressionQuality {
+    assert(compressionQuality <= 1.0 && compressionQuality >= 0);
+    
+    return UIImageJPEGRepresentation(self, compressionQuality);
+}
+
+- (CGFloat)compressionQuality {
+    NSData *data = UIImageJPEGRepresentation(self, 1.0);
+    NSUInteger dataLength = [data length];
+    
+    if(dataLength > MAX_IMAGEDATA_LEN) {
+        return 1.0 - MAX_IMAGEDATA_LEN / dataLength;
+    } else {
+        return 1.0;
+    }
+}
+
+- (NSData *)compressedData {
+    CGFloat quality = [self compressionQuality];
+    
+    return [self compressedData:quality];
+}
 
 
 @end;
